86a9765a61644cde7fdf5e3847d6e12a69c811e6
Bryce Long <longbryc@gmail.com>
2022-01-21 14:35:07 -0800

    Add the Xenon files

---
+++ keyboards/xenon/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+CONSOLE_ENABLE = yes       # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
+SPLIT_KEYBOARD = yes       # Split common
+OLED_ENABLE = yes          # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
